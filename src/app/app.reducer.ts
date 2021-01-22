ac7c773994e864ec8f92b2b790168ed66da942aa
Kaj Białas <kaj.bialas@dev-industry.com>
2021-01-22 02:39:09 +0100

    Add localstorage redux store

--- src/app/app.reducer.ts
+++ src/app/app.reducer.ts
@@ -5,11 +5,19 @@ const rootReducer: Reducer = combineReducers({
   home: homeReducer.reducer,
 });
 
+const localStorageState = localStorage.getItem('reduxState');
+const preloadedState = localStorageState ? JSON.parse(localStorageState) : {};
+
 export const createStore = () =>
   configureStore({
     reducer: rootReducer,
+    preloadedState,
   });
 
 export const store = createStore();
+
+store.subscribe(() => {
+  localStorage.setItem('reduxState', JSON.stringify(store.getState()));
+});
 
 export default rootReducer;
--- src/app/home/home.component.tsx
+++ src/app/home/home.component.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import homeSlice from './home.reducer';
 import Content from '../../components/content/content.component';
 
 const Home: React.FC = () => {
-  return <Content>HomePage</Content>;
+  const { setDate } = homeSlice.actions;
+  const dispatch = useDispatch();
+
+  return (
+    <Content>
+      <button
+        type="button"
+        onClick={() => {
+          dispatch(setDate('new'));
+        }}
+      >
+        test
+      </button>
+    </Content>
+  );
 };
 
 export default Home;
